cdf2b8584a89ee19090dcb0c9b122c40443a998e
unknown <kamalelsayed118@gmail.com>
2025-08-10 04:31:21 +0300

    add product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, ProductImage, Brand, Category, ProductReview
+
+
+class ProductImageTabular(admin.TabularInline):
+    model = ProductImage
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageTabular]
+    list_display = ('name', 'skl', 'flag', 'quantity', 'brand', 'category', 'price')
+    list_filter = ('flag', 'brand', 'category', 'price')
+    search_fields = ('name','desc','subtite', 'skl', 'brand__name', 'category__name')
+
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage)  
+admin.site.register(ProductReview)             
+admin.site.register(Brand)
+admin.site.register(Category)
+
--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "taggit",
+
+
+
     'accounts',
     'orders',
     'products',
